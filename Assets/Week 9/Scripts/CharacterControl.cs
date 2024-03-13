dc8047e4cedd4bd291b39a0ccf4512db088c6186
cartergi <cartergi@sheridancollege.ca>
2024-03-13 16:01:27 -0400

    Task 4b Complete: I can't figure out how to get it without the error

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public static CharacterControl Instance;
+    public TextMeshProUGUI textMeshy;
+    Type villagerType;
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -15,5 +19,15 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+
+    private void Update()
+    {
+        villagerType = SelectedVillager.GetType();
+        textMeshy.text = villagerType.ToString();
+        if (SelectedVillager == null)
+        {
+            textMeshy.text = "None Selected";
+        }
+    }
+
 }
